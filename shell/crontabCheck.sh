215dd97abf383ca7bcaa649dc5e80a6b483c98bf
yzbz <44864350+yzbz@users.noreply.github.com>
2020-09-08 17:41:38 +0800

    add append format

--- shell/crontabCheck.sh
+++ shell/crontabCheck.sh
@@ -2,6 +2,8 @@
 #echo -e "当前日期是 `date`" >> record.txt
 #/usr/bin/curl http://api.***.cn:88/Crontab/testUserEmpty  >> record.txt
 #echo "this is mailtest" | mail -s "hello" 523631013@qq.com
+#>>追加
+#>修改
 str=`curl http://api.***.cn:88/Crontab/handleUserNameEmpty`
 curTime=`date +"%Y-%m-%d %H-%M-%S"`
 echo $curTime"     "$str >> /root/test/test_php/record.txt
