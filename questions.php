7f2553c95702df75716b3333641a8295f9b71dcf
yzbz <44864350+yzbz@users.noreply.github.com>
2020-10-13 11:02:26 +0800

    Create questions.php

---
+++ questions.php
@@ -0,0 +1,30 @@
+<?php
+
+$str = 'abcdefghigklmn';
+$str1 = 'abcdefabcdeabcdabcaba';
+$arrStr = 'abc,def,gh';
+//strpos
+//stripos
+//strstr
+//strtr
+//substr
+//substr_count
+//str_pad
+//round
+//ceil
+//floor
+
+//explode
+//implode
+//array_diff
+//array_keys
+//array_values
+//array_flip
+//array_pop
+//array_push
+//array_shift
+//array_unshift
+//array_walk
+//array_map
+//array_filter
+>
